ecdebb3a6f33585428eba75be2db9fa4f7dcf6b8
suwatjanee005 <42610547+suwatjanee005@users.noreply.github.com>
2019-11-18 11:30:42 +0700

    Update j0902.java

--- w10/10.2/j0902.java
+++ w10/10.2/j0902.java
@@ -2,11 +2,11 @@ import java.io.*;
 public class j0902 {
   public static void main (String args[]) throws IOException {
     int i = 0,t1,t2;
-    String b,CatID;
+    String b,categoryid;
     String[] fields;
     String[] recs1 = new String[30];
     String[] recs2 = new String[30];
-    FileReader fin = new FileReader("categories.txt");
+    FileReader fin = new FileReader("products.txt");
     BufferedReader bin = new BufferedReader (fin);
     while ((b =  bin.readLine()) != null) {
       recs1[i] = b;
@@ -15,8 +15,8 @@ public class j0902 {
     fin.close();
     t1 = i;
     i = 0;
-    //
-    FileReader fin2 = new FileReader("products.txt");
+
+    FileReader fin2 = new FileReader("categories.txt");
     BufferedReader bin2 = new BufferedReader (fin2);
     while ((b =  bin2.readLine()) != null) {
       recs2[i] = b;
@@ -25,15 +25,15 @@ public class j0902 {
     }
     fin2.close();
     t2 = i;
-    //
+
     for(int j=0;j<t1;j++) {
       fields = recs1[j].split(",");
-      System.out.println("Show categories"+" = "+fields[0] +" , "+ fields[1] +" , "+  fields[2]);
-     CatID = fields[0];
-      for(int k=0;k<t2;k++) {
-        fields = recs2[k].split(",");
-        if (fields[3].equals(CatID)) {
-           System.out.println("Show products"+" = "+fields[0]+" , "+ fields[1]+" , "+ fields[2]+" , "+ fields[3]+", "+  fields[4]);
+      System.out.println("Show Products"+" = "+fields[0] +" , "+ fields[1] +" , "+  fields[2]+" , "+  fields[3]+" , "+  fields[4]);
+     categoryid = fields[3];
+      for(int p=0;p<t2;p++) {
+        fields = recs2[p].split(",");
+        if (fields[0].equals(categoryid)) {
+          System.out.println("Show categories"+" = "+fields[2]);
 
         }
       }
